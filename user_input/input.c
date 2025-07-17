7756f5b57df6a5818675806666c239c6dbb709fa
bishalkalicote369 <bishalkalicotecode@gmail.com>
2025-07-17 14:05:24 +0545

    input

---
+++ user_input/input.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  int age = 0;
+  float gpa = 0.0f;
+  char grade = '\0';
+  char name[30] = "";
+  printf("%d\n", age);
+  printf("%.2f\n", gpa);
+  printf("%c\n", grade);
+
+  printf("%s\n", name);
+
+  return 0;
+}
