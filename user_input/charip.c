e02af11eb6820665358a446e78d7f3c84bf53037
bishalkalicote369 <bishalkalicotecode@gmail.com>
2025-07-18 13:29:13 +0545

    input char

---
+++ user_input/charip.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+
+  char ch;
+
+  printf("Enter a single character: ");
+  scanf(" %c", &ch);
+
+  printf("You entered character : %c\n", ch);
+
+  return 0;
+}
